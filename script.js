a5bd1c3699391f293a7ddf1a3afb8685c2187d0b
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-07-25 12:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //your JS code here. If required.
-function myFunction(){
-	document.getElementById("status").innerHTML=<h1> Entered Metaverse </h1>
-}
+// function myFunction(){
+// 	document.getElementById("status").innerHTML=<h1> Entered Metaverse </h1>
+// }
+ 
+let status = document.getElementById('status');
+let enterBtn = document.getElementById('enterBtn');
+
+// Add event listener to the button
+enterBtn.addEventListener('click', function() {
+  // Change the text of the p tag
+  status.innerText = 'Entered Metaverse';
+});
